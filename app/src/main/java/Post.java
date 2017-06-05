8e73c2b5566421df1c1f4871eaaca4216a969aa5
dkfrayne <dkfrayne@gmail.com>
2017-06-04 17:56:56 -0700

    LocalEvents class with event post data, post_layout xml file

--- app/src/main/java/Post.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by dkfrayne on 6/4/17.
- */
-
-public class Post {
-    
-    
-}
--- app/src/main/java/com/omicronrobotics/rafaelszuminski/localify/MainActivity.java
+++ app/src/main/java/com/omicronrobotics/rafaelszuminski/localify/MainActivity.java
@@ -1,14 +1,29 @@
 package com.omicronrobotics.rafaelszuminski.localify;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity{
 
+    TextView mContentView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_bar_main);
+        
+        mContentView = (TextView) findViewById(R.id.content_post);
+        
+        String[] currentPosts = LocalEvents.getFakeLocalEvents(30);
+        
+        for(String post : currentPosts) {
+            mContentView.append(post);
+        }
+        
     }
 
 
