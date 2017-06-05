6b60450fb34a01a9df47ced98b8b60631fcd71ad
Rafael <rafcin.s@gmail.com>
2017-06-04 17:57:34 -0700

    Added Search Bar

--- app/src/main/java/com/omicronrobotics/rafaelszuminski/localify/MainActivity.java
+++ app/src/main/java/com/omicronrobotics/rafaelszuminski/localify/MainActivity.java
@@ -2,14 +2,44 @@ package com.omicronrobotics.rafaelszuminski.localify;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+
+import com.yalantis.jellytoolbar.listener.JellyListener;
+import com.yalantis.jellytoolbar.widget.JellyToolbar;
 
 public class MainActivity extends AppCompatActivity{
+
+    private JellyToolbar toolbar;
+    private AppCompatEditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app_bar_main);
+        setContentView(R.layout.test_layout);
+
+        toolbar = (JellyToolbar) findViewById(R.id.toolbar);
+        //toolbar.getToolbar().setNavigationIcon(R.drawable.ic_searchdark);
+        toolbar.setJellyListener(jellyListener);
+
+
+        editText = (AppCompatEditText) LayoutInflater.from(this).inflate(R.layout.edit_text, null);
+        editText.setBackgroundResource(R.color.colorTransparent);
+        toolbar.setContentView(editText);
+
     }
+
+    private JellyListener jellyListener = new JellyListener() {
+        @Override
+        public void onCancelIconClicked() {
+            if (TextUtils.isEmpty(editText.getText())) {
+                toolbar.collapse();
+            } else {
+                editText.getText().clear();
+            }
+        }
+    };
 
 
 }
