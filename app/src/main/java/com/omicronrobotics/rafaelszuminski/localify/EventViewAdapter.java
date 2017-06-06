df965d47d256d36f90e6ea08109da3c8845cbaa0
Rafael <rafcin.s@gmail.com>
2017-06-06 13:00:52 -0700

    UI Updates Part2

--- app/src/main/java/com/omicronrobotics/rafaelszuminski/localify/EventViewAdapter.java
+++ app/src/main/java/com/omicronrobotics/rafaelszuminski/localify/EventViewAdapter.java
@@ -3,9 +3,11 @@ package com.omicronrobotics.rafaelszuminski.localify;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,7 +24,8 @@ public class EventViewAdapter extends RecyclerView.Adapter{
     Context adapContext;
     LayoutInflater layoutInflater;
 
-    private String mURL = "https://lh3.googleusercontent.com/proxy/A6pVXalBNZtPf_Og75YROwW1cOJwaPKebBzAerfWw9y2mqBSoezvfjtac5tUg9IJS_46AXuRJQNswlAx-mj3EmMRZOqzCkNvUNIOcMoSQMchCPhPc_-teg=s1060-p-rw";
+    private String mPostImageadr = "https://thenextweb.com/wp-content/blogs.dir/1/files/2013/09/85807485.jpg";
+    private String mUserIconadr = "https://lh6.googleusercontent.com/-0sRw6H119gw/AAAAAAAAAAI/AAAAAAAAAAA/wBS2KABxWnY/s128-c-k/photo.jpg";
 
 
 
@@ -30,12 +33,32 @@ public class EventViewAdapter extends RecyclerView.Adapter{
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView eventName;
         ImageView imageFile;
+        ImageView userIcon;
+        ImageButton shareButton;
+        Toolbar mTopToolBar;
+        Toolbar mBottomToolBar;
+
+
+
+
         public MyViewHolder(View v) {
             super(v);
 
             eventName = (TextView) v.findViewById(R.id.eventName);
             imageFile = (ImageView)v.findViewById(R.id.img_thumbnail);
+            userIcon = (ImageView)v.findViewById(R.id.userIcon);
+            shareButton = (ImageButton) v.findViewById(R.id.shareColorButton);
+            mTopToolBar = (Toolbar)v.findViewById(R.id.detailToolbar);
 
+            mTopToolBar.setTitleTextColor(Color.WHITE);
+            mTopToolBar.setBackgroundColor(Color.WHITE);
+            mTopToolBar.setTitle("");
+
+            mBottomToolBar = (Toolbar)v.findViewById(R.id.bottomBar);
+
+            mBottomToolBar.setTitleTextColor(Color.WHITE);
+            mBottomToolBar.setBackgroundColor(Color.WHITE);
+            mBottomToolBar.setTitle("");
 
             eventName.setTextColor(Color.rgb(33,33,33));   //BlueColor -- Color.rgb(46,68,159)
         }
@@ -60,9 +83,27 @@ public class EventViewAdapter extends RecyclerView.Adapter{
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         MyViewHolder vHolder = (MyViewHolder) holder;
+        vHolder.shareButton.setColorFilter(Color.argb(255, 255, 255, 255)); // White Tint
         vHolder.eventName.setText(events.get(position));
-        //vHolder.imageFile.setImageResource();
-        Picasso.with(adapContext).load(mURL).into(vHolder.imageFile);
+        vHolder.mTopToolBar.setTitle(events.get(position));
+
+
+        vHolder.userIcon.setAdjustViewBounds(true);
+        Picasso
+                .with(adapContext)
+                .load(mUserIconadr)
+                .resize(41,41) // resizes the image to these dimensions (in pixel)
+                .centerCrop()
+                .into(vHolder.userIcon);
+        //Picasso.with(adapContext).load(mPostImageadr).into(vHolder.imageFile);
+        vHolder.imageFile.setAdjustViewBounds(true);
+        Picasso
+                .with(adapContext)
+                .load(mPostImageadr)
+                .resize(3500,3500) // resizes the image to these dimensions (in pixel)
+                .centerCrop()
+                .into(vHolder.imageFile);
+
 
     }
 
